e8679ebeaf2dd13f02e6be12ba03b8b8a1cd83ee
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-06-16 08:54:39 -0300

    first commit

---
+++ SoulCode/src/main/java/SoulCode/Services/ServicesApplication.java
@@ -0,0 +1,15 @@
+package SoulCode.Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesApplication.class, args);
+		
+		System.out.println("allan tavares"); 
+	}
+
+}
