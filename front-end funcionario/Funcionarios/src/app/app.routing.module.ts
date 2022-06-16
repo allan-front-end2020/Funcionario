168c2b2c9d55442ff5e0bb47075eb18aa5aea77c
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-06-16 13:44:00 -0300

    adicionado roteamento no front

---
+++ front-end funcionario/Funcionarios/src/app/app.routing.module.ts
@@ -0,0 +1,23 @@
+import{NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule , Routes} from '@angular/router'
+
+
+const routes: Routes = [
+
+]
+
+@NgModule({
+    declarations:[
+
+    ],
+    imports:[
+      CommonModule
+    ],
+    providers:[
+
+
+    ]
+})
+export class AppRoutingModule {}
+
