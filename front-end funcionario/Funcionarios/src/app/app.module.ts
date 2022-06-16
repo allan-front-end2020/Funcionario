89f3f2bef0e01349ab16f7c00db74afa5363a038
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-06-16 15:03:33 -0300

    feita estruturas de rotas no front

--- front-end funcionario/Funcionarios/src/app/app.module.ts
+++ front-end funcionario/Funcionarios/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule} from './app.routing.module'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end funcionario/Funcionarios/src/app/app.routing.module.ts
+++ front-end funcionario/Funcionarios/src/app/app.routing.module.ts
@@ -1,10 +1,21 @@
 import{NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule , Routes} from '@angular/router'
+import {CommonModule} from '@angular/common';
+import {RouterModule  , Routes} from '@angular/router'
+import { ReturnStatement } from '@angular/compiler';
+
 
 
 const routes: Routes = [
-
+    {
+        path: 'funcionario',
+         loadChildren: () => {
+            return import('./funcionario/funcionario.module')
+           .then((m) =>{
+            return m.FuncionarioModule
+           
+    })
+   }
+}
 ]
 
 @NgModule({
@@ -12,8 +23,14 @@ const routes: Routes = [
 
     ],
     imports:[
-      CommonModule
+      CommonModule,
+      RouterModule.forRoot(routes)
+
     ],
+    exports:[
+        RouterModule
+    ],
+
     providers:[
 
 
