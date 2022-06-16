a231394233535914743855c8911c4a493bfe1373
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-06-16 10:59:15 -0300

    estrutura angular inicializada e material instalado

---
+++ front-end funcionario/Funcionarios/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
